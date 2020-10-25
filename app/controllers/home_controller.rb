93e28c478c91f3c1809ff0be65328784f9eee013
Tommy-Mai <72852271+Tommy-Mai@users.noreply.github.com>
2020-10-25 16:53:42 +0900

    create home controller,pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  def top
+  end
+
+  def about
+  end
+
+  def policy
+  end
+
+  def contact
+  end
+  
+  def faqs
+  end
+  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/' => "home#top"
+  get "about" => "home#about"
+  get "policy" => "home#policy"
+  get "contact" => "home#contact"
+  get "faqs" => "home#faqs"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
