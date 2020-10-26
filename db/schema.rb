1cc3d591e9d4ca3d39ac580f1ae20a2eef872b65
Tommy-Mai <72852271+Tommy-Mai@users.noreply.github.com>
2020-10-26 13:45:02 +0900

    create meal_tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_084403) do
+ActiveRecord::Schema.define(version: 2020_10_26_043526) do
+
+  create_table "meal_tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "meal_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", limit: 30, null: false
