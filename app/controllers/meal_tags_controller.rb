80da511edb8e743bfab2aa15f1db8a8c9bc19d16
Tommy-Mai <72852271+Tommy-Mai@users.noreply.github.com>
2020-11-01 17:15:49 +0900

    Revert "カレンダー機能追加"

--- app/controllers/meal_tags_controller.rb
+++
@@ -1,8 +0,0 @@
-class MealTagsController < ApplicationController
-  
-  def index
-    @meal_tags = MealTag.find_by(id: params[:id])
-    @meal_tasks = MealTask.where(meal_tag_id: @meal_tags.id).order(date: :desc,time: :desc)
-  end
-  
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'home#top'
-  resources :meal_tasks
-
-  get 'calendar/index'
-
-  get "meal_tags/:id/index" => "meal_tags#index"
-
-  get "about" => "home#about"
-  get "policy" => "home#policy"
-  get "contact" => "home#contact"
-  get "faqs" => "home#faqs"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
