a42deb26e16bdae28bb757796311269e318fab2a
Tommy-Mai <72852271+Tommy-Mai@users.noreply.github.com>
2020-11-17 11:18:16 +0900

    fix codes with rubocop

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,8 +4,13 @@ class CalendarController < ApplicationController
   def index
     @date = params[:start_date].to_date
     # 現在の月+前後一ヶ月のデータを取得し、正しくカレンダーにタスク数が表示されるように絞り込み
-    @meal_tasks = current_user.meal_tasks.where("date >= :last_month AND date <= :next_month",
-      {last_month: @date.last_month.beginning_of_month, next_month: @date.next_month.end_of_month})
+    @meal_tasks = current_user.meal_tasks.where(
+      "date >= :last_month AND date <= :next_month",
+      {
+        last_month: @date.last_month.beginning_of_month,
+        next_month: @date.next_month.end_of_month
+      }
+    )
   end
 
   def show
