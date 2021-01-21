fd0ea67fac1d69c25edf609d990d57f1fa25e4a5
Tommy-Mai <72852271+Tommy-Mai@users.noreply.github.com>
2021-01-21 22:44:28 +0900

    create new branch for AWS server

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:Tommy-Mai/MyDailyLife.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, 'main'
+set :branch, 'master'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/rails/MyDailyLife"
